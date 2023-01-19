fef0206798c55dafc3048cfe13115323c014e077
Stoumee Ghosh <sghosh@cnsonline.net>
2023-01-19 00:01:57 +0000

    new project

---
+++ src/Demo/Wed.java
@@ -0,0 +1,13 @@
+package Demo;
+
+public class Wed {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Stoumee";
+	     String LastName = "Ghosh";
+	     System.out.println("My Name Is "+ name + LastName);
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sghosh
+ *
+ */
+module Testrep {
+}
