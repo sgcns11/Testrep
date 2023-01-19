6edf53d998e3a67e10a94808e4bba5f4fc1f8387
Stoumee Ghosh <sghosh@cnsonline.net>
2023-01-19 00:27:30 +0000

    new change is Branch-2

--- src/Demo/Wed.java
+++ src/Demo/Wed.java
@@ -6,7 +6,7 @@ public class Wed {
 		// TODO Auto-generated method stub
 		String name = "Stoumee";
 	     String LastName = "Ghosh";
-	     System.out.println("My Name Is "+ name + LastName);
+	     System.out.println("My Name "+ name + LastName);
 
 	}
 
