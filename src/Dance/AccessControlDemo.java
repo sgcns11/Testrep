ecfe1495053c671359e1cedc2af0083a6fa21c6e
Stoumee <sgcns11@gmail.com>
2023-02-05 15:15:16 +0000

    Program on access control

---
+++ src/Dance/AccessControlDemo.java
@@ -0,0 +1,21 @@
+package Dance;
+
+public class AccessControlDemo {
+	
+	int a; // default access
+    public int b; //public access
+    private int c;//private access
+    void setC(int i)
+    {
+    	c=i;
+    }
+    int getc()
+    {
+    	return c;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+	}
+
+}
---
+++ src/Dance/AccessCrontroTest.java
@@ -0,0 +1,20 @@
+package Dance;
+
+public class AccessCrontroTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    AccessControlDemo Obj1 = new AccessControlDemo();
+    Obj1.a=5;
+    Obj1.b=6;
+    Obj1.setC(8);
+    System.out.println("Vlaue of A:"+ Obj1.a);
+    System.out.println("Vlaue of B:"+Obj1.b);
+    System.out.println("Vlaue of C:"+Obj1.getc());
+    //when a member of a class is public then it is accessed by any other code
+    //If private then getter and setter need to used as it not accessable to any other classes or sub classes. .
+    // Can be used only the by the members of the class thats is getters and setter. 
+    //protected applies only when inheritance is involved
+	}
+
+}
