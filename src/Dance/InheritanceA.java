bcb76c296e12ea5af43c1c94b194468d78889c55
Stoumee <sgcns11@gmail.com>
2023-02-05 16:18:27 +0000

    Demo on Inheritance

---
+++ src/Dance/InheritanceA.java
@@ -0,0 +1,10 @@
+package Dance;
+
+public class InheritanceA {
+	int i,j;
+	void showij()
+	{
+		System.out.println("value of i and j:"+ i+","+j);
+	}
+
+}
---
+++ src/Dance/InheritanceB.java
@@ -0,0 +1,10 @@
+package Dance;
+
+public class InheritanceB extends InheritanceA{
+	int k;
+	void showk()
+	{
+		System.out.println("Valueof K:"+k);
+	}
+
+}
